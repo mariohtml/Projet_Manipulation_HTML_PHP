eddc04a380db58843532e3af7c950d4ea556fd5a
Marius Ost <o.marius@hotmail.fr>
2020-03-30 15:15:35 +0100

    init projet

---
+++ script.php
@@ -0,0 +1,18 @@
+<?php
+
+
+is_file ( string $fichier.cvs ) : bool
+
+
+var_dump(is_file('fichier.txt')) . "\n";
+var_dump(is_file('F:\MES_COURS_CCI\Damien Kinol_BIEN_BIEN\Projet_Manipulation_HTML_Tableau_CVS_PHP')) . "\n";
+
+
+
+
+
+
+
+
+
+?>
---
+++ tableau.js
@@ -0,0 +1,13 @@
+	var jours;
+		jours = new Array(1); //jours=[];
+		jours[0]= 'Lundi';
+		jours[1]= 'Mardi';
+		jours[2]= 'Mercredi';
+		jours[3]= 'Jeudi';
+		jours[4]= 'Vendredi';
+		jours[5]= 'Samedi';
+		jours[6]= 'dimanche';
+
+	console.log(jours);
+	console.table(jours);
+	console.log(jours[2]);
